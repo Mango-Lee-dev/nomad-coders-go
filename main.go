8510a0ca7c5a2dcf218046e7bfec6f8c59f7d489
wootaiklee <fined000@naver.com>
2026-03-08 16:56:04 +0900

    Implement URL checking in main; replace dictionary search with HTTP requests to multiple URLs

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-
-	"github.com/wootaiklee/side-project/dict"
+	"net/http"
 )
 
+var errRequestFailed = errors.New("Request failed")
+
 func main() {
-	dictionary := dict.Dictionary{"first": "first word"}
-	definition, err := dictionary.Search("second")
-	if err != nil {
-		fmt.Println(err)
+	var results = make(map[string]string)
+	urls := []string{
+		"https://www.google.com",
+		"https://www.naver.com",
+		"https://www.daum.net",
+		"https://www.yahoo.com",
+		"https://www.bing.com",
+		"https://www.ask.com",
+		"https://www.duckduckgo.com",
+		"https://www.yahoo.com",
+		"https://www.bing.com",
 	}
-	fmt.Println(definition)
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
 }	
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+	fmt.Println("Response:", resp.StatusCode)
+	return nil
+}
