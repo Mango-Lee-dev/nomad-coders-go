fd4ab010d83951f176c6897be2289c017d88e605
wootaiklee <fined000@naver.com>
2026-03-08 16:29:20 +0900

    Refactor main to use Dictionary; implement Search method for word lookup

--- main.go
+++ main.go
@@ -3,18 +3,14 @@ package main
 import (
 	"fmt"
 
-	"github.com/wootaiklee/side-project/banking"
+	"github.com/wootaiklee/side-project/dict"
 )
 
 func main() {
-	account := banking.NewAccount("John Doe")
-	account.Deposit(100)
-	account.Deposit(100)
-	err := account.Withdraw(250)
+	dictionary := dict.Dictionary{"first": "first word"}
+	definition, err := dictionary.Search("second")
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(account.Balance(), account.Owner())
-	account.ChangeOwner("Jane Doe1")
-	fmt.Println(account.Owner())
+	fmt.Println(definition)
 }	
